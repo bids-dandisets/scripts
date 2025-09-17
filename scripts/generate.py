4d3673204343c0178fbb41c50fd9c0de8203509e
Cody Baker <cody.c.baker.phd@gmail.com>
2025-09-17 11:49:25 -0400

    feat: first version of script

--- scripts/generate.py
+++ scripts/generate.py
@@ -7,22 +7,24 @@ import subprocess
 import dandi.dandiapi
 import nwb2bids
 
-GITHUB_TOKEN = os.environ.get('GITHUB_TOKEN', None)
+GITHUB_TOKEN = os.environ.get("GITHUB_TOKEN", None)
 if GITHUB_TOKEN is None:
     message = "GITHUB_TOKEN environment variable not set"
     raise ValueError(message)
 
-BASE_GITHUB_URL = "https://api.github.com/repos/bids-dandisets"
+BASE_GITHUB_URL = "https://api.github.com/repos"
 BASE_DIRECTORY = pathlib.Path("E:/GitHub/bids-dandisets")
+
 
 def create_github_repo(repo_name: str) -> dict:
     """Create a new GitHub repository."""
-    url = f'https://api.github.com/user/repos'
-    headers = {'Authorization': f'token {GITHUB_TOKEN}'}
-    data = {'name': repo_name}
+    url = "https://api.github.com/user/repos"
+    headers = {"Authorization": f"token {GITHUB_TOKEN}"}
+    data = {"name": repo_name}
     response = requests.post(url, headers=headers, json=data)
     response.raise_for_status()
     return response.json()
+
 
 def run(limit: int | None = None) -> None:
     client = dandi.dandiapi.DandiapiClient()
@@ -34,19 +36,22 @@ def run(limit: int | None = None) -> None:
 
         dandiset_id = dandiset.id
 
-        dataset_converter = nwb2bids.DatasetConverter.from_remote_dandiset(dandiset_id=dandiset_id)
+        dataset_converter = nwb2bids.DatasetConverter.from_remote_dandiset(
+            dandiset_id=dandiset_id
+        )
         if len(dataset_converter.session_converters) == 0:
             print(f"No NWB files found in Dandiset {dandiset_id}, skipping...")
 
             continue
 
-        repo_url = f"{BASE_GITHUB_URL}/{dandiset_id}"
+        repo_name = f"bids-dandisets/{dandiset_id}"
+        repo_url = f"{BASE_GITHUB_URL}/{repo_name}"
         response = requests.get(repo_url)
         if response.status_code == 404:
             print(f"Creating GitHub repository for Dandiset {dandiset_id}...")
 
-            headers = {'Authorization': f'token {GITHUB_TOKEN}'}
-            data = {'name': repo_name}
+            headers = {"Authorization": f"token {GITHUB_TOKEN}"}
+            data = {"name": repo_name}
             response = requests.post(repo_url, headers=headers, json=data)
             response.raise_for_status()
 
@@ -54,17 +59,31 @@ def run(limit: int | None = None) -> None:
         if not repo_directory.exists():
             print(f"Cloning GitHub repository for Dandiset {dandiset_id}...")
 
-            subprocess.run(args=['git', 'clone', repo_url], cwd=BASE_DIRECTORY)
+            subprocess.run(args=["git", "clone", repo_url], cwd=BASE_DIRECTORY)
 
         dataset_converter.extract_metadata()
         dataset_converter.convert_to_bids_dataset(bids_directory=repo_directory)
 
-        subprocess.run(args=["git", "config", "--local", "user.email", '"github-actions[bot]@users.noreply.github.com"'], cwd=repo_directory)
-        subprocess.run(args=["git", "config", "--local", "user.name ", '"github-actions[bot]"'], cwd=repo_directory)
-        subprocess.run(args=['git', 'add', "."], cwd=repo_directory)
-        subprocess.run(args=['git', 'commit', '--message', '"update"'], cwd=repo_directory)
-        subprocess.run(args=['git', 'push'], cwd=repo_directory)
+        subprocess.run(
+            args=[
+                "git",
+                "config",
+                "--local",
+                "user.email",
+                '"github-actions[bot]@users.noreply.github.com"',
+            ],
+            cwd=repo_directory,
+        )
+        subprocess.run(
+            args=["git", "config", "--local", "user.name ", '"github-actions[bot]"'],
+            cwd=repo_directory,
+        )
+        subprocess.run(args=["git", "add", "."], cwd=repo_directory)
+        subprocess.run(
+            args=["git", "commit", "--message", '"update"'], cwd=repo_directory
+        )
+        subprocess.run(args=["git", "push"], cwd=repo_directory)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     run(limit=2)
