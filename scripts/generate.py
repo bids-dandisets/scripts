422c404f15cdacb2955a479a64c7c1b18f00c68f
Cody Baker <cody.c.baker.phd@gmail.com>
2025-09-18 16:08:32 -0400

    fix: various improvements

--- scripts/generate.py
+++ scripts/generate.py
@@ -12,7 +12,7 @@ import requests
 import nwb2bids
 
 LIMIT_SESSIONS = 2
-LIMIT_DANDISETS = 4
+LIMIT_DANDISETS = None
 
 GITHUB_TOKEN = os.environ.get("GITHUB_TOKEN", None)
 if GITHUB_TOKEN is None:
@@ -75,12 +75,16 @@ def run(limit: int | None = None) -> None:
         dandiset_id = dandiset.identifier
         repo_directory = BASE_DIRECTORY / dandiset_id
 
+        print(f"Processing Dandiset {dandiset_id}...")
         repo_name = f"bids-dandisets/{dandiset_id}"
         repo_url = f"{BASE_GITHUB_URL}/{repo_name}"
         repo_api_url = f"{BASE_GITHUB_API_URL}/{repo_name}"
-        response = requests.get(repo_api_url)
-        if response.status_code == 404:
-            print(f"Creating GitHub repository for Dandiset {dandiset_id}...")
+        response = requests.get(url=repo_api_url)
+        if response.status_code != 200:
+            print(f"Status code {response.status_code}: {response.content}\n\tCreating GitHub repository...")
+
+            if response.status_code == 403:
+                continue
 
             repo_creation_url = "https://api.github.com/orgs/bids-dandisets/repos"
             headers = {"Authorization": f"token {GITHUB_TOKEN}", "Accept": "application/vnd.github+json"}
@@ -90,8 +94,11 @@ def run(limit: int | None = None) -> None:
                 "default_branch": "draft",
                 "auto_init": True,
                 "description": f"BIDS-formatted version of Dandiset {dandiset_id}.",
+                "has_issues": False,
+                "has_projects": False,
+                "has_wiki": False,
             }
-            response = requests.post(repo_creation_url, headers=headers, json=data)
+            response = requests.post(url=repo_creation_url, headers=headers, json=data)
             response.raise_for_status()
 
         if not repo_directory.exists():
@@ -116,7 +123,7 @@ def run(limit: int | None = None) -> None:
         collections.deque((shutil.rmtree(path=path, ignore_errors=True) for path in current_content), maxlen=0)
 
         dataset_converter = nwb2bids.DatasetConverter.from_remote_dandiset(
-            dandiset_id=dandiset_id, limit=LIMIT_DANDISETS
+            dandiset_id=dandiset_id, limit=LIMIT_SESSIONS
         )
         dataset_converter.extract_metadata()
         dataset_converter.convert_to_bids_dataset(bids_directory=repo_directory)
@@ -149,7 +156,7 @@ def run(limit: int | None = None) -> None:
         _deploy_subprocess(command='git commit --message "update"', cwd=repo_directory, ignore_errors=True)
         _deploy_subprocess(command=f"git push --set-upstream origin {commit_hash}", cwd=repo_directory)
 
-        print(f"\nProcess complete for Dandiset {dandiset_id}!\n\n")
+        print(f"Process complete for Dandiset {dandiset_id}!\n\n")
 
 
 if __name__ == "__main__":
