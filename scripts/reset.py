604aaceee2c79c13727e75eb76fcba00352f4d10
Cody Baker <51133164+CodyCBakerPhD@users.noreply.github.com>
2025-09-19 02:07:03 -0400

    Various improvements (#2)
    * fix: various improvements
    
    * fix: various improvements
    
    * fix: various improvements
    
    * fix: various improvements
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * fix: various improvements
    
    * fix: various improvements
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

---
+++ scripts/reset.py
@@ -0,0 +1,79 @@
+import os
+import pathlib
+import shutil
+import subprocess
+
+import dandi.dandiapi
+import nwb2bids
+import requests
+
+GITHUB_TOKEN = os.environ.get("GITHUB_TOKEN", None)
+if GITHUB_TOKEN is None:
+    message = "GITHUB_TOKEN environment variable not set"
+    raise ValueError(message)
+
+BASE_GITHUB_API_URL = "https://api.github.com/repos"
+BASE_DIRECTORY = pathlib.Path("E:/GitHub/bids-dandisets")
+
+
+def _deploy_subprocess(
+    *,
+    command: str | list[str],
+    cwd: str | pathlib.Path | None = None,
+    environment_variables: dict[str, str] | None = None,
+    error_message: str | None = None,
+    ignore_errors: bool = False,
+) -> str | None:
+    error_message = error_message or "An error occurred while executing the command."
+
+    result = subprocess.run(
+        args=command,
+        cwd=cwd,
+        shell=True,
+        env=environment_variables,
+        capture_output=True,
+        text=True,
+        encoding="utf-8",
+    )
+    if result.returncode != 0 and ignore_errors is False:
+        message = (
+            f"\n\nError code {result.returncode}\n"
+            f"{error_message}\n\n"
+            f"stdout: {result.stdout}\n\n"
+            f"stderr: {result.stderr}\n\n"
+        )
+        raise RuntimeError(message)
+    if result.returncode != 0 and ignore_errors is True:
+        return None
+
+    return result.stdout
+
+
+def reset_github_repos() -> None:
+    commit_hash = _deploy_subprocess(command="git rev-parse HEAD", cwd=pathlib.Path(nwb2bids.__file__).parents[1])[:10]
+    print(f"\nnwb2bids commit hash: {commit_hash}\n\n")
+
+    client = dandi.dandiapi.DandiAPIClient()
+    dandisets = client.get_dandisets()
+
+    for dandiset in dandisets:
+        dandiset_id = dandiset.identifier
+        repo_directory = BASE_DIRECTORY / dandiset_id
+
+        print(f"Cleaning Dandiset {dandiset_id}...")
+        print("Cleaning local directory...")
+        if repo_directory.exists():
+            shutil.rmtree(path=repo_directory)
+
+        repo_name = f"bids-dandisets/{dandiset_id}"
+        repo_api_url = f"{BASE_GITHUB_API_URL}/{repo_name}"
+        response = requests.get(url=repo_api_url)
+        if response.status_code == 200:
+            print("Cleaning repository...")
+            headers = {"Authorization": f"token {GITHUB_TOKEN}"}
+            requests.delete(url=repo_api_url, headers=headers)
+            print("Cleaning complete!\n\n")
+
+
+if __name__ == "__main__":
+    reset_github_repos()
