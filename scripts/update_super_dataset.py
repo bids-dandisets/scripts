fe0c0431cb14ab1b68d1fbe42e65488f208c0dd9
Cody Baker <cody.c.baker.phd@gmail.com>
2025-10-06 14:05:30 -0400

    feat: update

--- scripts/update_super_dataset.py
+++ scripts/update_super_dataset.py
@@ -11,7 +11,7 @@ import dandi.dandiapi
 import nwb2bids
 import requests
 
-LIMIT_SESSIONS = None
+LIMIT_SESSIONS = 5
 LIMIT_DANDISETS = None
 
 GITHUB_TOKEN = os.environ.get("GITHUB_TOKEN", None)
@@ -29,27 +29,22 @@ raw_content_base_url = "https://raw.githubusercontent.com/bids-dandisets"
 BASE_DIRECTORY = pathlib.Path("E:/GitHub/bids-dandisets")
 authentication_header = {"Authorization": f"token {GITHUB_TOKEN}"}
 
-# Config is likely temporary to suppress the 'unknown version' because we run from BEP32 schema
-BIDS_VALIDATION_CONFIG_FILE_PATH = pathlib.Path(__file__).parent / "bids_validation_config.json"
-
 
 def run(limit: int | None = None) -> None:
     script_repo_path = pathlib.Path(__file__).parents[1]
 
     version_tag_command = "git describe --tags --always"
-    generation_script_version = _deploy_subprocess(command=version_tag_command, cwd=script_repo_path).strip()
-    print(f"\nGeneration script version: {generation_script_version}")
+    generation_script_version_tag = _deploy_subprocess(command=version_tag_command, cwd=script_repo_path).strip()
+    print(f"\nGeneration script version: {generation_script_version_tag}")
 
     nwb2bids_repo_path = pathlib.Path(nwb2bids.__file__).parents[1]
-    nwb2bids_version = _deploy_subprocess(command=version_tag_command, cwd=nwb2bids_repo_path).strip()
-    print(f"nwb2bids version: {nwb2bids_version}\n\n")
+    nwb2bids_version_tag = _deploy_subprocess(command=version_tag_command, cwd=nwb2bids_repo_path).strip()
+    print(f"nwb2bids version: {nwb2bids_version_tag}\n\n")
 
     run_info = {
-        "generation_script_version": generation_script_version,
-        "nwb2bids_version": nwb2bids_version,
+        "generation_script_version_tag": generation_script_version_tag,
+        "nwb2bids_version_tag": nwb2bids_version_tag,
         "limit": LIMIT_SESSIONS,
-        "sessions_converted": None,
-        "total_sessions": None,
     }
 
     client = dandi.dandiapi.DandiAPIClient()
@@ -94,8 +89,8 @@ def run(limit: int | None = None) -> None:
             previous_nwb2bids_version_tag = previous_run_info.get("nwb2bids_version_tag", "")
             previous_session_limit = previous_run_info.get("limit", 0)
             if (
-                previous_generation_script_version_tag == generation_script_version
-                and nwb2bids_version == previous_nwb2bids_version_tag
+                previous_generation_script_version_tag == generation_script_version_tag
+                and nwb2bids_version_tag == previous_nwb2bids_version_tag
                 and LIMIT_SESSIONS <= previous_session_limit
             ):
                 print(f"Skipping {dandiset_id} - already up to date!\n\n")
@@ -123,21 +118,18 @@ def run(limit: int | None = None) -> None:
         dataset_converter.extract_metadata()
 
         print("Updating draft...")
-
         _write_bids_dandiset(dataset_converter=dataset_converter, repo_directory=repo_directory, run_info=run_info)
+
         _push_changes(repo_directory=repo_directory, branch_name="draft")
 
-        # TODO: only make other branches for config options like sanitization
-        # try:
-        #     _deploy_subprocess(command=f"git checkout -b {nwb2bids_version}", cwd=repo_directory)
-        # except RuntimeError:
-        #     _deploy_subprocess(command=f"git checkout {nwb2bids_version}", cwd=repo_directory)
-        #
-        #     print("\tUpdating commit branch...")
-        #     _write_bids_dandiset(
-        #         dataset_converter=dataset_converter, repo_directory=repo_directory, run_info=run_info
-        #     )
-        # _push_changes(repo_directory=repo_directory, branch_name=nwb2bids_version)
+        try:
+            _deploy_subprocess(command=f"git checkout -b {nwb2bids_version_tag}", cwd=repo_directory)
+        except RuntimeError:
+            _deploy_subprocess(command=f"git checkout {nwb2bids_version_tag}", cwd=repo_directory)
+
+            print("\tUpdating commit branch...")
+            _write_bids_dandiset(dataset_converter=dataset_converter, repo_directory=repo_directory, run_info=run_info)
+        _push_changes(repo_directory=repo_directory, branch_name=nwb2bids_version_tag)
 
         print(f"Process complete for Dandiset {dandiset_id}!\n\n")
 
@@ -178,20 +170,18 @@ def _deploy_subprocess(
 def _write_bids_dandiset(
     dataset_converter: nwb2bids.DatasetConverter, repo_directory: pathlib.Path, run_info: dict
 ) -> None:
-    nwb2bids_info_directory = repo_directory / ".nwb2bids"
-    nwb2bids_info_directory.mkdir(exist_ok=True)
-    run_info_file_path = nwb2bids_info_directory / "run_info.json"
+    run_info_file_path = repo_directory / ".run_info.json"
 
     bids_ignore_file_path = repo_directory / ".bidsignore"
     derivatives_directory = repo_directory / "derivatives"
     derivatives_dataset_description_file_path = derivatives_directory / "dataset_description.json"
-    validations_directory = derivatives_directory / "validations"
-    nwb2bids_notifications_file_path = validations_directory / "nwb2bids_notifications.json"
+    inspections_directory = derivatives_directory / "inspections"
+    nwb2bids_inspection_file_path = inspections_directory / "nwb2bids_inspection.json"
     # nwb_inspector_version = importlib.metadata.version(distribution_name="nwbinspector").replace(".", "-")
     # nwb_inspection_file_path = inspections_directory / f"src-nwb-inspector_ver-{nwb_inspector_version}.txt"
-    bids_validation_file_path = validations_directory / "bids_validation.txt"
-    bids_validation_json_file_path = validations_directory / "bids_validation.json"
-    # dandi_validation_file_path = validations_directory / "dandi_validation.txt"
+    bids_validation_file_path = inspections_directory / "bids_validation.txt"
+    bids_validation_json_file_path = inspections_directory / "bids_validation.json"
+    dandi_validation_file_path = inspections_directory / "dandi_validation.txt"
 
     # Cleanup existing content from original fork or previous runs
     current_content = [path for path in repo_directory.iterdir() if not path.name.startswith(".") and path.is_dir()]
@@ -200,7 +190,11 @@ def _write_bids_dandiset(
     if derivatives_directory.exists():
         shutil.rmtree(path=derivatives_directory)
     derivatives_directory.mkdir(exist_ok=True)
-    validations_directory.mkdir(exist_ok=True)
+    inspections_directory.mkdir(exist_ok=True)
+
+    # TODO: write dataset_description.json and README for inspections pipeline
+    # TODO: write dataset_description.json and README for entire 'study'
+    # QUESTION FOR YARIK: does this repo itself need to be nested under a 'study-<label>' directory?
 
     dataset_converter.convert_to_bids_dataset(bids_directory=repo_directory)
 
@@ -216,9 +210,9 @@ def _write_bids_dandiset(
     }
     derivatives_dataset_description_file_path.write_text(json.dumps(obj=derivatives_dataset_description))
 
-    notifications_dump = [notification.model_dump(mode="json") for notification in dataset_converter.messages]
-    if len(notifications_dump) > 0:
-        nwb2bids_notifications_file_path.write_text(data=json.dumps(obj=notifications_dump, indent=2))
+    message_dump = [message.model_dump(mode="json") for message in dataset_converter.messages]
+    if len(message_dump) > 0:
+        nwb2bids_inspection_file_path.write_text(data=json.dumps(obj=message_dump, indent=2))
 
     # if nwb_inspection_file_path.exists() is False:
     #     dandiset_id = repo_directory.name
@@ -230,47 +224,26 @@ def _write_bids_dandiset(
     bids_validator_command = (
         f"bids-validator-deno --ignoreNiftiHeaders --verbose --outfile {bids_validation_file_path} "
         "--schema https://raw.githubusercontent.com/bids-standard/bids-schema/enh-prs-and-beps/BEPs/32/schema.json "
-        f"--config {BIDS_VALIDATION_CONFIG_FILE_PATH} "
         f"{repo_directory}"
     )
-    _deploy_subprocess(command=bids_validator_command, ignore_errors=True)  # Annoyingly always returns 1 on warnings
+    _deploy_subprocess(command=bids_validator_command, ignore_errors=True)
 
     bids_validator_json_command = (
         f"bids-validator-deno --ignoreNiftiHeaders --verbose --json --outfile {bids_validation_json_file_path} "
         "--schema https://raw.githubusercontent.com/bids-standard/bids-schema/enh-prs-and-beps/BEPs/32/schema.json "
-        f"--config {BIDS_VALIDATION_CONFIG_FILE_PATH} "
         f"{repo_directory}"
     )
     _deploy_subprocess(command=bids_validator_json_command, ignore_errors=True)
-
     with bids_validation_json_file_path.open(mode="r") as file_stream:
         content = json.load(fp=file_stream)
     with bids_validation_json_file_path.open(mode="w") as file_stream:
         json.dump(obj=content, fp=file_stream, indent=2)
 
-    # _deploy_subprocess(command=f"dandi validate {repo_directory} > {dandi_validation_file_path}", ignore_errors=True)
+    _deploy_subprocess(command=f"dandi validate {repo_directory} > {dandi_validation_file_path}", ignore_errors=True)
 
     # Write last as a sign of completion
-    # Temporary
-    old_file_path = repo_directory / ".run_info.json"
-    old_file_path.unlink(missing_ok=True)
-
-    dandiset_run_info = run_info.copy()
-    session_subdirectories = {
-        path
-        for path in repo_directory.rglob(pattern="*ses-*")
-        if path.is_dir() and len(set(path.rglob(pattern="*.nwb"))) > 0
-    }
-    dandiset_run_info["sessions_converted"] = len(session_subdirectories)
-    if LIMIT_SESSIONS is None:
-        dandiset_run_info["total_sessions"] = len(dataset_converter.session_converters)
-    elif LIMIT_SESSIONS is not None and dandiset_run_info["sessions_converted"] < LIMIT_SESSIONS:
-        dandiset_run_info["total_sessions"] = f"{LIMIT_SESSIONS}+"
-    elif LIMIT_SESSIONS is not None and dandiset_run_info["sessions_converted"] == LIMIT_SESSIONS:
-        dandiset_run_info["total_sessions"] = "???"
-
     with run_info_file_path.open(mode="w") as file_stream:
-        json.dump(obj=dandiset_run_info, fp=file_stream, indent=2)
+        json.dump(obj=run_info, fp=file_stream, indent=2)
 
 
 def _configure_git_repo(repo_directory: pathlib.Path) -> None:
